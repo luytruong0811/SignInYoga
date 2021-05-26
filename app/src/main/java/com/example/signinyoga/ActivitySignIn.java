d2c78ea3155e6989dc8e73a0fbd99b302dde7497
= <=>
2021-05-26 19:56:53 +0700

    complete Activity Sign In

--- app/src/main/java/com/example/signinyoga/ActivitySignIn.java
+++ app/src/main/java/com/example/signinyoga/ActivitySignIn.java
@@ -1,14 +1,28 @@
 package com.example.signinyoga;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivitySignIn extends AppCompatActivity {
+    private ConstraintLayout clSignInWithEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        clSignInWithEmail = findViewById(R.id.cl_sign_in_with_email);
+        clSignInWithEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivitySignIn.this, SignInWithEmail.class);
+                startActivity(intent);
+            }
+        });
     }
 }
