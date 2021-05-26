20c61398dbd96e1fa234d6eca54814a998e0a24d
= <=>
2021-05-26 17:41:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/signinyoga/ActivitySignIn.java
@@ -0,0 +1,14 @@
+package com.example.signinyoga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
