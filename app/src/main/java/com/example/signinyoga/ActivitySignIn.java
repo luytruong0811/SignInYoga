90ba1baee81ff038372148fe3b4acfede61e43d0
= <=>
2021-05-26 23:07:11 +0700

    commit edit

--- app/src/main/java/com/example/signinyoga/ActivitySignIn.java
+++ app/src/main/java/com/example/signinyoga/ActivitySignIn.java
@@ -1,29 +1,23 @@
 package com.example.signinyoga;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.LinearLayout;
 
 public class ActivitySignIn extends AppCompatActivity {
-    private LinearLayout clSignInWithEmail;
+    private LinearLayout lnSignInWithEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        clSignInWithEmail = findViewById(R.id.cl_sign_in_with_email);
-        clSignInWithEmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ActivitySignIn.this, SignInWithEmail.class);
-                startActivity(intent);
-            }
+        lnSignInWithEmail = findViewById(R.id.ln_sign_in_with_email);
+        lnSignInWithEmail.setOnClickListener(v -> {
+            Intent intent = new Intent(ActivitySignIn.this, SignInWithEmail.class);
+            startActivity(intent);
         });
     }
 }
