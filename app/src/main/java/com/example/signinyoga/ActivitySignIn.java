4732a326942f7f1c4b10ec9decbc5cfa03e5d427
Hung Nguyen <hungnguyencntt2808@gmail.com>
2021-05-27 08:54:06 +0700

    fix

--- app/src/main/java/com/example/signinyoga/ActivitySignIn.java
+++ app/src/main/java/com/example/signinyoga/ActivitySignIn.java
@@ -6,11 +6,16 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 public class ActivitySignIn extends AppCompatActivity {
     private LinearLayout clSignInWithEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,7 @@ public class ActivitySignIn extends AppCompatActivity {
         setContentView(R.layout.activity_signin);
 
         clSignInWithEmail = findViewById(R.id.cl_sign_in_with_email);
+
         clSignInWithEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +31,7 @@ public class ActivitySignIn extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+
 }
--- app/src/main/java/com/example/signinyoga/SignInWithEmail.java
+++ app/src/main/java/com/example/signinyoga/SignInWithEmail.java
@@ -3,12 +3,59 @@ package com.example.signinyoga;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.text.method.SingleLineTransformationMethod;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignInWithEmail extends AppCompatActivity {
-
+    private CheckBox cbPassword;
+    private EditText etPassword;
+    private ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        cbPassword = findViewById(R.id.cb_password);
+        etPassword = findViewById(R.id.et_set_password);
+        iv = findViewById(R.id.iv_hide);
         setContentView(R.layout.activity_sign_in_with_email);
+//        cbPassword.setChecked(true);
+//
+//        cbPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                int start,end;
+//                if(!isChecked){
+//                    start=etPassword.getSelectionStart();
+//                    end=etPassword.getSelectionEnd();
+//                    etPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());;
+//                    etPassword.setSelection(start,end);
+//                }else{
+//                    start=etPassword.getSelectionStart();
+//                    end=etPassword.getSelectionEnd();
+//                    etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+//                    etPassword.setSelection(start,end);
+//                }
+//            }
+//        });
+
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etPassword.getTransformationMethod().getClass().getSimpleName().equals("PasswordTransformationMethod")) {
+                    etPassword.setTransformationMethod(new SingleLineTransformationMethod());
+                    iv.setImageResource(R.drawable.ic_show);
+                }
+                else {
+                    etPassword.setTransformationMethod(new PasswordTransformationMethod());
+                    iv.setImageResource(R.drawable.ic_hide);
+                }
+                etPassword.setSelection(etPassword.getText().length());
+            }
+        });
     }
 }
